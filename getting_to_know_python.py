79ea7dcc339868bbf2b08405a5e27de9b4c70f89
Neziswa1 <neziswatotti@gmail.com>
2019-11-25 09:45:23 +0200

    final submission

--- getting_to_know_python.py
+++ getting_to_know_python.py
@@ -1,6 +1,9 @@
 function_count = 0
 function_dict = {}
 function_name =[]
+start = []
+end = []
+
 
 f = open("information.js")
 for line in f:
@@ -18,4 +21,25 @@ for line in f:
                 function_call = function[0]
                 function_dict['name'] = function_call
                 print(function_dict)
+                #path_to_js_file = open
+                if "function" in line and "}" in line:
+                end.append(counter)              
+                f.close()
+                equals_index = function_name.index("=")
+                if equals_index != -1:
+                   function_name.remove("=")
+                return [start,function_name,end]
+x = list_all_js_function_names("/home/recruit/get_to_know_python/Untitled-123.py")
+
+start = x[0]
+names = x[1]
+
+output = {}
+output_ls = []
+
+for i, num in enumerate(start):
+    output = {"name": names[i], "start": num}
+    output_ls.append(output)
+
+print(output_ls)
 f.close()
